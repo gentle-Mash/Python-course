c4d1dff5456c2782f61f29f28393db703ec423bc
gentle-Mash <mashrafisikder02@gmail.com>
2023-05-21 15:04:39 +0600

    coffee machine updated

--- beginer projects/coffee machine/index.py
+++ beginer projects/coffee machine/index.py
@@ -30,9 +30,10 @@ MENU = {
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+    "water": 3000,
+    "milk": 2000,
+    "coffee": 1000,
+    "money": 0,
 }
 
 quarters = 0.25
@@ -66,9 +67,12 @@ def total_amount(): # money processing function
 
     return total_price
 
+def machine_money_count(coffee_name): # keep track of total sell
+    resources["money"] += MENU[coffee_name]["cost"]
+
 
 def is_sufficient(coffee_name): # ingredients checking function
-    if (resources["water"] > MENU[coffee_name]["ingredients"]["water"]) and (resources["milk"] > MENU[coffee_name]["ingredients"]["milk"]) and (resources["coffee"] > MENU[coffee_name]["ingredients"]["coffee"]):
+    if (resources["water"] >= MENU[coffee_name]["ingredients"]["water"]) and (resources["milk"] >= MENU[coffee_name]["ingredients"]["milk"]) and (resources["coffee"] >= MENU[coffee_name]["ingredients"]["coffee"]):
         return True
     return False
 
@@ -81,7 +85,11 @@ def making_coffee(coffee_name):  # main coffee maker function
         change = user_given_amount - MENU[coffee_name]["cost"]
         print(f"Here is your change ${round(change, 3)} and your {coffee_name}.Enjoy!")
         for key in resources:
-            resources[key] = resources[key] - MENU[coffee_name]["ingredients"][key] 
+            if key != "money":
+                resources[key] = resources[key] - MENU[coffee_name]["ingredients"][key]
+            else:
+                machine_money_count(coffee_name)
+
 
 
 def coffee_maker(coffee_name): # permission to make coffee function
@@ -101,12 +109,14 @@ def coffee_maker(coffee_name): # permission to make coffee function
 #************************************************* main code **************************************************
 def working():
     while True:
-        print("Welcome to my coffee shop. what you would like to try? here are 3 flavours of Hot Coffee. 'Espresso(1)' or 'Latte(2)' or 'Cappuccino(3)' ")
+        print("Welcome to my coffee shop. what you would like to try? here are 3 flavours of Hot Coffee. 'Espresso- $ 1.5 (1)' or 'Latte- $ 2.5 (2)' or 'Cappuccino- $ 3 (3)' ")
         user_choice = input("what's your choice?: ").strip()
         if user_choice == "report":
             for key in resources:
                 if key == "coffee":
                     print(f"{key} : {resources[key]} gm")
+                elif key == "money":
+                    print(f"{key} : $ {resources[key]}")
                 else:
                     print(f"{key} : {resources[key]} mL")
             continue
@@ -118,6 +128,9 @@ def working():
             break
         elif user_choice == "3":
             coffee_maker("cappuccino")
+            break
+        elif user_choice == "off":
+            print("Shutting down")
             break        
         else:
             print("Type 1 or 2 or 3")
