55d73047a8084e1477c3fcbfd811394a95aa7455
gentle-Mash <mashrafisikder02@gmail.com>
2023-05-25 16:37:40 +0600

    classic snake game added

---
+++ beginer projects after all the chapter is completed/snake game/scoreboard.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+FONT = "Arial"
+ALIGNMENT = "center"
+FONT_SIZE = 15
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('white')
+        self.penup()
+        self.goto(0,270)
+        self.write(f"Score: {self.score} ", False, ALIGNMENT, (FONT, FONT_SIZE, "bold"))
+        self.hideturtle()
+    
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.write(f"Score: {self.score} ", False, ALIGNMENT, (FONT, FONT_SIZE, "bold"))
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"Game Over", False, ALIGNMENT, (FONT, FONT_SIZE, "bold"))
+
+    def restart(self):
+        self.clear()
+        self.score = 0
+        self.goto(0,270)
+        self.write(f"Score: {self.score} ", False, ALIGNMENT, (FONT, FONT_SIZE, "bold"))
+        
+
--- chapter 25 turtle graphics/1 draw a square/index.py
+++ chapter 25 turtle graphics/1 draw a square/index.py
@@ -13,6 +13,7 @@ my_turtle.left(90)
 my_turtle.fd(100)
 my_turtle.left(90)
 
+
 # instead of these many lines of code, you could also code like this
 
 # for i in range(4):
