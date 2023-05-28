6d82df6fa3fea2406f7f55ed8b20ab045e2dc657
gentle-Mash <mashrafisikder02@gmail.com>
2023-05-28 22:19:39 +0600

    chapter 19 updated

--- chapter 19 working with files/exercise 1/index.py
+++ chapter 19 working with files/exercise 1/index.py
@@ -2,4 +2,4 @@ with open("salary.txt","r") as rf:
     with open("new_output.txt", "a") as wf:
         for line in rf.readlines():
             name, salary = line.split(",")
-            wf.write(f"{name}'s saly is{salary}")
+            wf.write(f"{name}'s salary is{salary}")
